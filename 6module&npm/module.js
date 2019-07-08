130a2ed1964a1f9ad3a7d13066c28b69c9e86215
kitety <aizaizuori@foxmail.com>
2019-07-08 23:56:08 +0800

    npm更新

--- 6module&npm/module.js
+++ 6module&npm/module.js
@@ -29,5 +29,38 @@ js没有模块系统，不支持封闭的作用域和依赖管理
 
 // 4.2文件模块
 /**
- * 在硬盘
+ * 在硬盘某个位置，加载速度很慢，文件模块通过名称或路径来加载，文件模块后缀有三种
+ * .js 的脚本文件，先读入内存在运行
+ * .json json文件，读入内存，转换为json对象
+ * .node 经过编译后的二进制c/c++扩展模块文件，可以支架使用
+ * （一般自己的写的是通过路径加载，别人写的是通过名称去当前目录的node_modules寻找）
+ */
+
+//  4.3第三方模块
+// 使用require就只从node_modules寻找，这样不用修改路径。
+// 第三方模块的查询路径包括node_modules和全局目录
+
+// 4.3.1全局目录
+// window如果在环境变量中设置了NODE_PATH变量，并将变量设置为一个有效的磁盘目录，require在本地找不到此模块时向在此目录下找这个模块。 
+// UNIX操作系统中会从 $HOME/.node_modules $HOME/.node_libraries目录下寻找
+
+// 4.4模块的加载策略
+// 见图lookmodule.png
+
+// 4.5 文件模块查找规则
+// lookfile.png  递归查路径 添加后缀、缓存路径
+
+/* 5.从模块外部访问模块内部成员 */
+// exports对象
+// module.exports导出引用类型
+
+/* 6.模块对象的属性 */
+// http://www.ruanyifeng.com/blog/2015/05/require.html
+/**
+ * module.id
+ * module.filename
+ * module.loaded
+ * module.parent
+ * module.children
+ * module.paths
  */
