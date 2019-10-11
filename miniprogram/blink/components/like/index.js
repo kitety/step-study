3925a7b355c493e0b19897c04f5d7f76a6214250
刘正锟 <liuzhengkun@xiaoduotech.com>
2019-10-12 00:10:04 +0800

    feat:小程序组件封装

--- miniprogram/blink/components/like/index.js
+++ miniprogram/blink/components/like/index.js
@@ -3,21 +3,25 @@ Component({
   /**
    * 组件的属性列表
    */
-  properties: {
-
-  },
+  properties: {},
 
   /**
    * 组件的初始数据
    */
   data: {
-
+    like: false,
+    count1: 12,
+    count2: 123,
+    yesSrc: "./images/like.png",
+    noSrc: "./images/like@dis.png"
   },
 
   /**
    * 组件的方法列表
    */
   methods: {
-
+    onLike: e => {
+      console.log(this);
+    }
   }
-})
+});
