a90c0b2633bc374ab3853ee67bf6d33e930b7be2
刘正锟 <liuzhengkun@xiaoduotech.com>
2019-12-20 16:24:03 +0800

    feat: http更新

--- base/14http2/http.js
+++ base/14http2/http.js
@@ -1,9 +1,27 @@
 let http = require("http");
+let path = require("path");
+let crypto = require("crypto");
+
 let server = http
   .createServer(function(req, res) {
-    console.log(res.headersSent ? "响应头已经发送" : "响应头未发送");
-    res.writeHead(200, "ok");
-    console.log(res.headersSent ? "响应头已经发送" : "响应头未发送");
-    res.end()
+    res.writeHead(200, {
+      "Transfer-Encoding": "chunked",
+      Trailer: "Content-MD5"
+    });
+    let rs = require("fs").createReadStream(path.join(__dirname, "msg.txt"), {
+      highWaterMark: 200
+    });
+    let md5 = crypto.createHash("md5");
+    rs.on("data", function(data) {
+      console.log(data);
+      res.write(data);
+      md5.update(data);
+    });
+    rs.on("end", function() {
+      res.addTrailers({
+        "Content-MD5": md5.digest("hex")
+      });
+      res.end();
+    });
   })
-  .listen(8080, "127.0.0.1");
+  .listen(8080);
--- base/14http2/temp.js
+++ base/14http2/temp.js
@@ -1,16 +1,19 @@
 let http = require("http");
-let option = {
+let options = {
   hostname: "localhost",
   port: 8080,
   path: "/",
   method: "GET"
 };
-let req = http.request(option, res => {
-  console.log("code：" + res.statusCode);
-  console.log("headers: " + JSON.stringify(res.headers));
+let req = http.request(options, function(res) {
+  console.log("状态吗:" + res.statusCode);
+  console.log("响应头:" + JSON.stringify(res.headers));
   res.setEncoding("utf8");
-  res.on("data", chunk => {
-    console.log("content", chunk);
+  res.on("data", function(chunk) {
+    console.log("响应内容", chunk);
+  });
+  res.on("end", function() {
+    console.log("trailer", res.trailers);
   });
 });
-req.end()
+req.end();
