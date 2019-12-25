c5ff135cca507a2d456741d819639fa18f15c0c2
刘正锟 <liuzhengkun@xiaoduotech.com>
2019-12-26 00:22:24 +0800

    feat: 添加一个home组件

--- demos/ng-wyy/src/app/pages/pages.module.ts
+++ demos/ng-wyy/src/app/pages/pages.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
-import { ShareModule } from '../share/share.module';
+import { HomeModule } from './home/home.module';
 
 @NgModule({
   declarations: [],
   imports: [
-    // 每个页面都可以用到share module的模块
-    ShareModule
+    HomeModule
+  ],
+  exports: [
+    HomeModule
   ]
 })
 export class PagesModule { }
