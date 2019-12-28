951a598fe69f972a7bb87b618c107b10809f7a45
刘正锟 <liuzhengkun@xiaoduotech.com>
2019-12-28 23:44:10 +0800

    feat: 轮播图的触发

--- demos/ng-wyy/src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
+++ demos/ng-wyy/src/app/pages/home/components/wy-carousel/wy-carousel.component.ts
@@ -1,17 +1,26 @@
-import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild, Input, Output, EventEmitter, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-wy-carousel',
   templateUrl: './wy-carousel.component.html',
-  styleUrls: ['./wy-carousel.component.less']
+  styleUrls: ['./wy-carousel.component.less'],
+  // 变更检测策略
+  // 默认一个组件发生改变，会把那个组件树上关联的父子组件重新变更检测一遍，检测有无变化
+  // OnPush input输入的时候才会变更检测，否则其他组件变化但是不影响输入属性，就不会变化
+  // 性能提升关键
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WyCarouselComponent implements OnInit {
   // true变更检测前解析，false变更后
-  @ViewChild('dot', { static: true })
-  dotRef: TemplateRef<any>
+  @ViewChild('dot', { static: true }) dotRef: TemplateRef<any>
+  @Input() activeIndex = 0
+  @Output() changeSlide = new EventEmitter<'pre' | 'next'>()
   constructor() { }
 
   ngOnInit() {
+  }
+  onChangeSlide(type: 'pre' | 'next') {
+    this.changeSlide.emit(type)
   }
 
 }
--- demos/ng-wyy/src/app/pages/home/home.component.ts
+++ demos/ng-wyy/src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { HomeService } from 'src/app/services/home.service';
 import { Banner } from 'src/app/services/data-types/common.types';
+import { NzCarouselComponent } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-home',
@@ -9,7 +10,10 @@ import { Banner } from 'src/app/services/data-types/common.types';
 })
 export class HomeComponent implements OnInit {
   banners: Banner[]
-
+  // 激活的索引
+  carouselActiveIndex = 0
+  // 轮播组件实例
+  @ViewChild(NzCarouselComponent, { static: true }) private nzCarousel: NzCarouselComponent
   constructor(private homeService: HomeService) {
 
     this.homeService.getBanners().subscribe(banners => {
@@ -19,6 +23,13 @@ export class HomeComponent implements OnInit {
   }
 
   ngOnInit() {
+  }
+  onNzBeforeChange({ to }) {
+    this.carouselActiveIndex = to
+    console.log(to);
+  }
+  onChangeSlide(type: 'pre' | 'next') {
+    this.nzCarousel[type]()
   }
 
 }
