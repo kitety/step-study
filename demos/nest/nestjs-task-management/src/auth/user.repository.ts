91b11e592bcf100bc12edf424def7fc32ab4d3fb
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-02-09 12:32:33 +0800

    feat: nestjs 46 duplicate username

--- demos/nest/nestjs-task-management/src/auth/user.repository.ts
+++ demos/nest/nestjs-task-management/src/auth/user.repository.ts
@@ -1,14 +1,26 @@
 import { Repository, EntityRepository } from "typeorm";
 import { User } from "./user.entity";
 import { AuthCredentialDto } from "./dto/auth-credential.dto";
+import { ConflictException, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async signUp(authCredentialDto: AuthCredentialDto): Promise<void> {
     const { username, password } = authCredentialDto;
+    // find if username is already exist
+    // bad:send a query,just do it in entity
     const user = new User();
     user.username = username;
     user.password = password;
-    await user.save();
+    try {
+      await user.save();
+    } catch (error) {
+      if (error.code === "23505") {
+        // duplicate username
+        throw new ConflictException("username already exists");
+      }else{
+        throw new  InternalServerErrorException()
+      }
+    }
   }
 }
