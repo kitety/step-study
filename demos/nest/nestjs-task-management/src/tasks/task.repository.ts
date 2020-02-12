c87a98554e8710db601392332b531091b3bf0c07
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-02-12 12:21:36 +0800

    feat: nest 62 use logger

--- demos/nest/nestjs-task-management/src/tasks/task.repository.ts
+++ demos/nest/nestjs-task-management/src/tasks/task.repository.ts
@@ -4,9 +4,11 @@ import { EntityRepository, Repository } from "typeorm";
 import { CreateTaskDto } from "./dto/create-task.dto";
 import { TaskStatus } from "./task-status.enum";
 import { User } from "src/auth/user.entity";
+import { Logger, InternalServerErrorException } from "@nestjs/common";
 
 @EntityRepository(Task)
 export class TaskRepository extends Repository<Task> {
+  private logger = new Logger("TaskRepository");
   async getTasks(filterDto: GetTaskFilterDto, user: User): Promise<Task[]> {
     const { status, search } = filterDto;
     const query = this.createQueryBuilder("task");
@@ -27,8 +29,18 @@ export class TaskRepository extends Repository<Task> {
         }
       );
     }
-    const tasks = await query.getMany();
-    return tasks;
+    try {
+      const tasks = await query.getMany();
+      return tasks;
+    } catch (error) {
+      this.logger.error(
+        `Failed to get tasks for user ${user.username},DTO： ${JSON.stringify(
+          filterDto
+        )}`,
+        error.stack
+      );
+      throw new InternalServerErrorException();
+    }
   }
 
   async createTask(createTaskDto: CreateTaskDto, user: User): Promise<Task> {
@@ -38,7 +50,17 @@ export class TaskRepository extends Repository<Task> {
     task.description = description;
     task.status = TaskStatus.OPEN;
     task.user = user;
-    await task.save();
+    try {
+      await task.save();
+    } catch (error) {
+      this.logger.error(
+        `Failed to create task for user ${user.username},DTO： ${JSON.stringify(
+          createTaskDto
+        )}`,
+        error.stack
+      );
+      throw new InternalServerErrorException();
+    }
 
     delete task.user;
     return task;
--- demos/nest/nestjs-task-management/src/tasks/tasks.controller.ts
+++ demos/nest/nestjs-task-management/src/tasks/tasks.controller.ts
@@ -10,7 +10,8 @@ import {
   UsePipes,
   ValidationPipe,
   ParseIntPipe,
-  UseGuards
+  UseGuards,
+  Logger
 } from "@nestjs/common";
 import { TasksService } from "./tasks.service";
 import { CreateTaskDto } from "./dto/create-task.dto";
@@ -25,6 +26,7 @@ import { User } from "src/auth/user.entity";
 @Controller("tasks")
 @UseGuards(AuthGuard())
 export class TasksController {
+  private logger = new Logger("TasksController");
   constructor(private tasksService: TasksService) {}
   /**
    *
@@ -35,6 +37,7 @@ export class TasksController {
     @Query(ValidationPipe) filterDto: GetTaskFilterDto,
     @GetUser() user: User
   ): Promise<Task[]> {
+    this.logger.verbose(`User ${user.username}`);
     return this.tasksService.getTasks(filterDto, user);
   }
 
@@ -61,6 +64,7 @@ export class TasksController {
     @Body() createTaskDto: CreateTaskDto,
     @GetUser() user: User
   ): Promise<Task> {
+    this.logger.verbose(`User ${user.username}`);
     return this.tasksService.createTask(createTaskDto, user);
   }
   @Patch("/:id/:status")
