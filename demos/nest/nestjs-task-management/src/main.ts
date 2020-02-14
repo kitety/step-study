c829df25a49095ab6924548f242f3bfd96f985df
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-02-14 09:27:44 +0800

    feat: nestjs 64 config management set up

--- demos/nest/nestjs-task-management/src/main.ts
+++ demos/nest/nestjs-task-management/src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from "@nestjs/core";
 import { Logger } from "@nestjs/common";
 import { AppModule } from "./app.module";
+import * as config from 'config'
 
 async function bootstrap() {
   // bootstrap function
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
-  const port = 3000;
+  const serverConfig=config.get('server')
+  const { port } = serverConfig;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
 }
