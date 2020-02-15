be452b6235938ba1a1d14066540d813010630dc5
刘正锟 <liuzhengkun@xiaoduotech.com>
2020-02-15 10:49:10 +0800

    feat: nestjs 66 allow cors

--- demos/nest/nestjs-task-management/src/main.ts
+++ demos/nest/nestjs-task-management/src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const serverConfig = config.get("server");
   const logger = new Logger("bootstrap");
   const app = await NestFactory.create(AppModule);
+  if (process.env.NODE_ENV === "development") {
+    app.enableCors();
+  }
   const port = process.env.PORT || serverConfig.port;
   await app.listen(port);
   logger.log(`Application listening on port ${port}`);
